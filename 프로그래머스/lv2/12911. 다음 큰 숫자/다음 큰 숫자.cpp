eb5cb9362a41618fa0e9c639487a732666fb39de
HYJ <hyj765@naver.com>
2023-04-08 06:25:29 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int countOnes(int num) {
+    int count = 0;
+    while (num != 0) {
+        count += (num & 1);
+        num >>= 1;
+    }
+    return count;
+}
+int solution(int n) {
+    int answer = 0;
+    int cur = n;
+    int nCount = countOnes(n);
+    while(1){
+        cur++;
+        if(nCount == countOnes(cur)){
+            break;
+        }
+    }
+    answer = cur;
+    return answer;
+}
