1dde7549350717f17f0aae7a64dd9e760c734dfb
hwangyoungjun <hyj765@naver.com>
2023-10-12 13:21:46 +0900

    단속 카메라 upload

---
+++ 프로그래머스/lv3/단속카메라/단속카메라.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+/*
+
+   ---
+        -------
+    ------
+              ---
+
+
+*/
+
+bool compare(vector<int> a, vector<int> b) {
+
+    if (a[0] == b[0]) {
+        return a[1] < b[1];
+    }
+
+    return a[0] < b[0];
+}
+
+int solution(vector<vector<int>> routes) {
+    int answer = 1;
+
+    sort(routes.begin(), routes.end(), compare);
+    int out = routes[0][1];
+    for (vector<int> route : routes) {
+        if (route[0] > out) {
+            answer++;
+            out = route[1];
+        }
+
+        if (route[1] <= out) out = route[1];
+    }
+
+    return answer;
+}
