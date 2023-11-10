fa968a9a06906615dd027703f26e3f1a59c5c747
HYJ <hyj765@naver.com>
2023-11-10 14:58:24 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+#define max(a,b) (a > b) ? a : b
+
+int N;
+int arr[1001];
+int dp[1001];
+
+int main(void)
+{
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < N; i++)
+    {
+        dp[i] = arr[i];
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] < arr[i]) dp[i] = max(dp[i], dp[j] + arr[i]);
+        }
+    }
+
+    int ans = dp[0];
+    for (int i = 1; i < N; i++)
+        if (ans < dp[i]) ans = dp[i];
+
+    cout << ans;
+
+    return 0;
+}
