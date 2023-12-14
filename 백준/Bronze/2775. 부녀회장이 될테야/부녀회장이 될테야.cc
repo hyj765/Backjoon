c7e43f5949acec361cf96ebeca193c1dfede9316
HYJ <hyj765@naver.com>
2023-12-15 05:30:29 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -1,31 +1,48 @@
 #include<iostream>
-
-//a:b = f( a-1 + ... )
+#include<vector>
 
 using namespace std;
-// 1층 3호
-// 1 2 3 arr 0
-// 1 3 6 arr 1
-int main() {
-	//T 
-	// k n
-	// k n
-	int T, k, n;
-	cin >> T;
-	int dp[15][15] = {0,};
 
-	dp[0][0] = 0;
-	for (int i = 1; i < 15; ++i) {
+
+
+/*
+
+	dp[a][b] = dp[a-1][b]
+	
+	[1][3] = 6;
+		
+	[0][i] = 1 2 3 .....
+	[1][1] = 1;
+	[1][2] = 3;
+	[1][3] = 6;
+
+	dp[k][n] = dp[i - 1][n] + dp[i][n-1];
+
+	dp[k][n] = dp[i-1][n-1] .....
+
+*/
+
+int dp[15][15];
+
+int main() {
+	
+	int T,k, n;
+	cin >> T;
+	for (int i = 1; i <= 14; ++i) {
 		dp[0][i] = i;
 	}
 
-	for (int i = 1; i < 15; ++i) {
-		for (int j = 1; j < 15; ++j) {
-			dp[i][j] = dp[i][j - 1] + dp[i - 1][j]; 
+	for (int i = 1; i <= 14; ++i) {
+		for (int j = 1; j <= 14; ++j) {
+			dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
 		}
 	}
+
 	for (int i = 0; i < T; ++i) {
-		cin >> k >> n;
-		printf("%d\n", dp[k][n]);
+		cin >> k;
+		cin >> n;
+
+		cout << dp[k][n] << endl;
 	}
+
 }
