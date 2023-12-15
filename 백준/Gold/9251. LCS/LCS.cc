44b585b3f7e92a2a85ffebb59fd63c6b4f11502a
HYJ <hyj765@naver.com>
2023-12-15 13:58:09 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,25 +1,27 @@
 #include<iostream>
 #include<vector>
-#include<string>
 using namespace std;
 
-int n[1001][1001];
-
+int dp[1001][1001];
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	string a, b;
-	cin >> a;
-	cin >> b;
 
-	for (int i = 1; i <= a.size(); i++) {
-		for (int j = 1; j <= b.size(); j++) {
-			if (a[i - 1] == b[j - 1]) n[i][j] = n[i - 1][j - 1] + 1;
-			else {
-				n[i][j] = n[i - 1][j]> n[i][j - 1] ? n[i - 1][j] : n[i][j - 1];
+	string a, b;
+	cin >> a >> b;
+
+	for (int i = 1; i <= a.size(); ++i) {
+		for (int j = 1; j <= b.size(); ++j) {
+			if (a[i-1] == b[j-1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
 			}
+			else {
+				dp[i][j] = max(dp[i][j - 1],dp[i-1][j]);
+			}
+
+
 		}
 	}
-	cout << n[a.size()][b.size()];	
+
+
+
+	cout << dp[a.size()][b.size()];
 }
