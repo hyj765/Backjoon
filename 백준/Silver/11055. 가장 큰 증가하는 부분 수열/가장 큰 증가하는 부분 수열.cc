135b3b2f34b5cb649c820bed19603e0baf91d0c6
HYJ <hyj765@naver.com>
2023-12-17 18:01:04 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -1,32 +1,27 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-#define max(a,b) (a > b) ? a : b
 
-int N;
-int arr[1001];
 int dp[1001];
 
-int main(void)
-{
-    cin >> N;
+int main() {
 
-    for (int i = 0; i < N; i++)
-        cin >> arr[i];
+	int n,answer = 0;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; ++i) cin >> arr[i];
+	dp[0] = arr[0];
+	for (int i = 0; i <n; ++i) {
+		dp[i] = arr[i];
+		for (int j = 0; j < i; ++j) {
+			if (arr[i] > arr[j]) {
+				dp[i] = max(dp[i],dp[j]+arr[i]);
+			}
+		}
 
-    for (int i = 0; i < N; i++)
-    {
-        dp[i] = arr[i];
-        for (int j = 0; j < i; j++)
-        {
-            if (arr[j] < arr[i]) dp[i] = max(dp[i], dp[j] + arr[i]);
-        }
-    }
+		answer = max(answer, dp[i]);
+	}
 
-    int ans = dp[0];
-    for (int i = 1; i < N; i++)
-        if (ans < dp[i]) ans = dp[i];
+	cout << answer;
 
-    cout << ans;
-
-    return 0;
 }
