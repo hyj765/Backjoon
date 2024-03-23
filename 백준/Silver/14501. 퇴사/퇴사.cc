3e5ad6cf6e457a747329dfee3ce5fc57ea8854e2
HYJ <hyj765@naver.com>
2024-03-23 18:40:19 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -2,24 +2,36 @@
 #include<vector>
 using namespace std;
 
-int dp[16];
+int maxprofit = 0;
+void getsimualte(int depth, const vector<pair<int,int>> arr,int value,int n) {
+
+	if (depth >= n) {
+		maxprofit = max(value, maxprofit);
+		return;
+	}
+	if (depth + arr[depth].first <= n) {
+		getsimualte(depth + arr[depth].first, arr, value + arr[depth].second, n);
+	}
+	getsimualte(depth + 1, arr, value, n);
+}
+
+
+
+
 int main() {
-    int n;
-    cin >> n;
-    vector<pair<int, int>> ar(n + 1);
-    for (int i = 0; i < n; i++) {
-        cin >> ar[i].first >> ar[i].second;
-    }
 
-    for (int i = 0; i < n; i++) {
-        if (i + ar[i].first > n) continue;
-        if (dp[i + ar[i].first] < dp[i] + ar[i].second) {
-            dp[i + ar[i].first] = dp[i] + ar[i].second;
-            for (int j = i + ar[i].first; j <= n; j++) {
-                if (dp[j] < dp[i + ar[i].first]) dp[j] = dp[i + ar[i].first];
-            }
-        }
-    }
-    cout << dp[n];
+	int n;
+	cin >> n;
 
+	vector<pair<int,int>> arr(n);
+
+	for (int i = 0; i < n; ++i) {
+		int day, cost;
+		cin >> day >> cost;
+		arr[i].first = day;
+		arr[i].second = cost;
+	}
+
+	getsimualte(0, arr, 0, n);
+	cout << maxprofit;
 }
