7e9ba1143618f7219888aed1dff53a374ad9c5ab
HYJ <hyj765@naver.com>
2024-12-22 15:31:25 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include<numeric>
+using namespace std;
+
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    
+    if(number% n == 0 && number% m == 0)
+    {
+        answer = 1;
+    }
+    
+    
+    return answer;
+}
