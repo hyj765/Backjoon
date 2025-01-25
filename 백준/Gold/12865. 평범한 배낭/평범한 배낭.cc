e60dada551a2471cf6a057fe775aec1ff8be7e25
HYJ <hyj765@naver.com>
2025-01-26 06:45:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 36 ms, Memory: 42256 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,29 +1,44 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-int v[101];
-int w[101];
+int k[100001];
+int w[100001];
 
 int dp[101][100001];
 
-int main(){
-    
-    int n,m;
-    cin >> n >> m;
-    for(int i =1; i<=n; i++){
-        cin >> w[i] >> v[i];
-    }
-    
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
-            if(j-w[i] >= 0){
-                dp[i][j] = max(dp[i-1][j],dp[i-1][j-w[i]] +v[i]);
-            }else{
-                dp[i][j] = dp[i-1][j];
-            }
-        }
-    }
-    
-    cout << dp[n][m];
-    
+int main()
+{
+	int n, m;
+
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> k[i] >> w[i];
+	}
+
+	for (int i = 0; i <= n; ++i)
+	{
+		for (int j = 0; j <= m; ++j)
+		{
+			if (i == 0 || j == 0)
+			{
+				dp[i][j] = 0;
+				continue;
+			}
+			int weight = k[i - 1];
+			int value = w[i - 1];
+
+			if (j >= weight)
+			{
+				dp[i][j] = max(dp[i - 1][j - weight] + value, dp[i-1][j]);
+			}
+			else {
+				dp[i][j] = dp[i - 1][j];
+			}
+		}
+	}
+
+	cout << dp[n][m];
 }
